af27bb7b27aa680bd85c8177f32a39bba272df6b
Andy Chen <amazingandyyy@gmail.com>
2022-04-17 19:40:04 +0100

    docs: fix install script

--- install.sh
+++ install.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
 
-export version=$(cat VERSION)
+export version=0.0.1
 export author=amazingandyyy
-export package_name=$(cat NAME)
+export package_name=gofuzzyclone
 
 if [ -n "$1" ]; then
   version=$1
 fi
 
-echo "> installing package_name@$version"
-curl -LsO https://github.com/$author/package_name/archive/refs/tags/$version.zip &&
+echo "> installing $package_name@$version"
+curl -LsO https://github.com/$author/$package_name/archive/refs/tags/$version.zip &&
 unzip -o $version.zip &&
-rm -rf /opt/homebrew/bin/package_name &&
-sudo touch /opt/homebrew/bin/package_name &&
-chmod +x package_name-$version/bin/package_name &&
-mv -f package_name-$version/bin/package_name /opt/homebrew/bin
-rm -rf package_name-$version $version.zip
+rm -rf /opt/homebrew/bin/$package_name &&
+sudo touch /opt/homebrew/bin/$package_name &&
+chmod +x $package_name-$version/bin/$package_name &&
+mv -f $package_name-$version/bin/$package_name /opt/homebrew/bin
+rm -rf $package_name-$version $version.zip
 
-if ! [ -x "$(command -v package_name)" ]; then
-  echo 'Error: package_name failed to install' >&2
+if ! [ -x "$(command -v gofuzzyclone)" ]; then
+  echo 'Error: gofuzzyclone failed to install' >&2
   exit 1
 else
-  echo "> install package_name@$version successfully!"
-  eval($package_name -help)
+  echo "> install $package_name@$version successfully!"
+  gofuzzyclone -help
 fi
