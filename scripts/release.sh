66204bbc40abbfe5e6903b14a2460bb65df0d165
Andy Chen <amazingandyyy@gmail.com>
2022-04-18 11:34:44 +0100

    feat: use cmd

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,5 +3,5 @@
 export REPO_DIR="$(dirname "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )")"
 export package_name=$(basename $REPO_DIR)
 
-(set -x; go build $REPO_DIR/src/main.go)
+(set -x; go build $REPO_DIR/cmd/main.go)
 mv $REPO_DIR/main $REPO_DIR/bin/$package_name
